50c06992947a81f138827e6d9ce760405fa4faaf
Harry Baines <harryb0905@googlemail.com>
2017-02-14 20:41:13 +0000

    Made a shape class with starting variables

---
+++ Shape.java
@@ -0,0 +1,21 @@
+public class Shape
+{
+    
+    // fixed shape widths/heights
+    private int width = 30;
+    private int easyHeight = 10;
+    private int normalHeight = 50;
+    private int hardHeight = 100;
+    
+    // constructor
+    public Shape(int width, int height)
+    {
+        
+        
+        
+        
+    }
+
+
+
+}
