40d1cac21649eab71229aebd4eeef5c614987f21
Harry Baines <harryb0905@googlemail.com>
2017-02-15 18:51:31 +0000

    Created objects per level array in Level.java

--- Level.java
+++ Level.java
@@ -1,6 +1,11 @@
 public class Level {
 
-    private static final int[] = {20,30,40,50,75,100,250,500,750,1000};
+    // objects per level array
+    private static final int[] objPerLvl = {20,30,40,50,75,100,250,500,750,1000};
+    
+    
+    
+    
     
     
     
