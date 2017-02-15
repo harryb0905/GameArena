4b5f0236c099a1e6d5873e4f7fc3265e30650ffb
Harry Baines <harryb0905@googlemail.com>
2017-02-15 18:47:07 +0000

    Beginning Level.java file

---
+++ Level.java
@@ -0,0 +1,16 @@
+public class Level {
+
+    private static final int[] = {20,30,40,50,75,100,250,500,750,1000};
+    
+    
+    
+    // constructor
+    public Level()
+    {
+        
+        
+        
+    }
+
+
+}
