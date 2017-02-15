7603a245421753f8e5438d98c7daabc2a85909d6
Harry Baines <harryb0905@googlemail.com>
2017-02-15 18:44:14 +0000

    Added array of shapes to Driver class

--- Driver.java
+++ Driver.java
@@ -1,10 +1,16 @@
 public class Driver
 {
+    
+    // private static int noOfShapes = 30;
 
     public static void main(String[] args)
     {
         
         GameArena screen = new GameArena(700,400);
+        Shape[] shapes = new Shape[noOfShapes];
+        
+        
+        
         
     }
 
