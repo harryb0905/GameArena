1e882378652747e849faeddf7139959bd23fc312
Harry Baines <harryb0905@googlemail.com>
2017-02-16 21:58:09 +0000

    Made a menu file and attepmted a label

--- Driver.java
+++ Driver.java
@@ -18,10 +18,11 @@ public class Driver
         int startX = 1000;
         
         // new game
-        GameArena screen = new GameArena(1000,500);
         
         Level level = new Level();
         noOfBlocks = level.getNoOfBlocks(currentLevel);
+        
+        Menu menu = new Menu();
         
     
 //        // when user presses a button, start the game
@@ -39,50 +40,50 @@ public class Driver
 //        }
         
         
-        Block[] blocks = new Block[noOfBlocks];
-        Block groundBlock = new Block(1000, 50, 500, 475);
-        
-        // ground block
-        screen.addBlock(groundBlock);
-        
-        // debug
-        System.out.println("Shapes to be used on level " + currentLevel + " is: " + noOfBlocks);
-        
-        // generate heights and add to screen
-        for (int i = 0; i < noOfBlocks; i++)
-        {
-            randHeight = (int) (Math.random() * (5));
-            blocks[i] = new Block(20, randHeight, startX, 400, "WHITE");
-            System.out.println("Block has width: " + blocks[i].getWidth() + " and height: " + blocks[i].getHeight());
-            
-            startX += 400;
-            
-            // add block to screen
-            screen.addBlock(blocks[i]);
-        }
-        
-
-        // make blocks move
-        while (true)
-        {
-            for (int j = 0; j < noOfBlocks; j++)
-            {
-                // get the xPosition of the block
-                currentXPos = blocks[j].getXPosition();
-                
-                // get the dx value of the block
-                currentDx = blocks[j].getDx();
-                
-                // update the current x position of the block to existing pos + new x
-                currentXPos -= currentDx;
-                
-                // set xPosition of each block to new position
-                blocks[j].setXPosition(currentXPos);
-            }
-            
-            // pause the window
-            screen.pause();
-        }
+//        Block[] blocks = new Block[noOfBlocks];
+//        Block groundBlock = new Block(1000, 50, 500, 475);
+//        
+//        // ground block
+//        screen.addBlock(groundBlock);
+//        
+//        // debug
+//        System.out.println("Shapes to be used on level " + currentLevel + " is: " + noOfBlocks);
+//        
+//        // generate heights and add to screen
+//        for (int i = 0; i < noOfBlocks; i++)
+//        {
+//            randHeight = (int) (Math.random() * (5));
+//            blocks[i] = new Block(20, randHeight, startX, 400, "WHITE");
+//            System.out.println("Block has width: " + blocks[i].getWidth() + " and height: " + blocks[i].getHeight());
+//            
+//            startX += 400;
+//            
+//            // add block to screen
+//            screen.addBlock(blocks[i]);
+//        }
+//        
+//
+//        // make blocks move
+//        while (true)
+//        {
+//            for (int j = 0; j < noOfBlocks; j++)
+//            {
+//                // get the xPosition of the block
+//                currentXPos = blocks[j].getXPosition();
+//                
+//                // get the dx value of the block
+//                currentDx = blocks[j].getDx();
+//                
+//                // update the current x position of the block to existing pos + new x
+//                currentXPos -= currentDx;
+//                
+//                // set xPosition of each block to new position
+//                blocks[j].setXPosition(currentXPos);
+//            }
+//            
+//            // pause the window
+//            screen.pause();
+//        }
     
     }
 }
--- menu.java
+++ menu.java
@@ -6,5 +6,8 @@ public class Menu {
         // 3 buttons, title, background image constantly moving, music, labels
         // THIS IS SOME CHANGES ON THE MENU BRANCH
         
+        GameArena screen = new GameArena(1000,500);
+        
+    
     }
 }
